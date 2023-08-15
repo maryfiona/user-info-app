ba431fb1b5b3e28161e1337d61323020eb1c097a
maryfiona <aguchidimmamaryfiona@gmail.com>
2023-08-15 08:27:16 -0700

    initail commikt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    address: '',
+    dob: '',
+    gender: '',
+    image: null,
+  });
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleImageChange = (event) => {
+    const imageFile = event.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      image: imageFile,
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowPopup(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Full Name:
+          <input
+            type="text"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+  Email Address:
+  <input
+    type="email"
+    name="email"
+    value={formData.email}
+    onChange={handleInputChange}
+    required
+  />
+</label>
+<label>
+  Address:
+  <input
+    type="text"
+    name="address"
+    value={formData.address}
+    onChange={handleInputChange}
+    required
+  />
+</label>
+<label>
+  Date of Birth:
+  <input
+    type="date"
+    name="dob"
+    value={formData.dob}
+    onChange={handleInputChange}
+    required
+  />
+</label>
+<label>
+  Gender:
+  <select
+    name="gender"
+    value={formData.gender}
+    onChange={handleInputChange}
+    required
+  >
+    <option value="">Select Gender</option>
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+    <option value="other">Other</option>
+  </select>
+</label>
+<label>
+  <input
+    type="file"
+    name="image"
+    accept="image/*"
+    onChange={handleImageChange}
+    required
+  />
+</label>
+
+
+
+
+        
+        {/* Add similar input fields for other form fields */}
+        <button type="submit">Submit</button>
+      </form>
+
+{showPopup && (
+  <div className="PopupOverlay">
+    <div className="PopupContent">
+      <h2>Submitted Information:</h2>
+      <ul>
+        <li>
+          <strong>Full Name:</strong> {formData.fullName}
+        </li>
+        <li>
+          <strong>Email Address:</strong> {formData.email}
+        </li>
+        <li>
+          <strong>Address:</strong> {formData.address}
+        </li>
+        <li>
+          <strong>Date of Birth:</strong> {formData.dob}
+        </li>
+        <li>
+          <strong>Gender:</strong> {formData.gender}
+        </li>
+      </ul>
+      {formData.image && (
+  <div className="ImageContainer">
+    <img
+      src={URL.createObjectURL(formData.image)}
+      alt="User's chosen file"
+      className="ImagePreview"
+    />
+  </div>
+)}
+
+      <button className="CloseButton" onClick={() => setShowPopup(false)}>
+        Close
+      </button>
+    </div>
+  </div>
+)}
+
     </div>
   );
 }
